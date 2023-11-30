9569a125578c4c1cea72f0dcf31736b134872430
Stephen-Robbins <sw2tennis@gmail.com>
2023-11-30 09:28:32 -0800

    Merge master into main

---
+++ bit_diffusion2.py
@@ -0,0 +1,558 @@
+
+import math
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+from pathlib import Path
+from functools import partial
+from multiprocessing import cpu_count
+import torch
+from torch import nn, einsum
+from torch.special import expm1
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+
+from torch.optim import Adam
+from torchvision import transforms as T, utils
+
+from einops import rearrange, reduce, repeat
+from einops.layers.torch import Rearrange
+
+from PIL import Image
+from tqdm.auto import tqdm
+from ema_pytorch import EMA
+
+from accelerate import Accelerator
+import chess
+import chess.svg
+from IPython.display import SVG
+import numpy as np
+
+
+# Function to map chess pieces to numbers
+def piece_to_number(piece):
+    mapping = {'.': 0, 'P': 1, 'N': 2, 'B': 3, 'R': 4, 'Q': 5, 'K': 6,
+               'p': 7, 'n': 8, 'b': 9, 'r': 10, 'q': 11, 'k': 12}
+    return mapping.get(piece, 0)
+
+def fen_to_tensor(fen):
+    
+    # Consider only the piece placement part of the FEN string
+    piece_placement = fen.split()[0]
+
+    # Replace numbers in FEN with corresponding number of dots
+    for i in range(1, 9):
+        piece_placement = piece_placement.replace(str(i), '.' * i)
+    
+    # Create 8x8 chessboard
+    board = np.zeros((8, 8), dtype=int)
+    for r, row in enumerate(piece_placement.split('/')):
+        for c, piece in enumerate(row):
+            board[r, c] = piece_to_number(piece)
+    return board
+
+def number_to_piece(num):
+    mapping = {0: '.', 1: 'P', 2: 'N', 3: 'B', 4: 'R', 5: 'Q', 6: 'K',
+               7: 'p', 8: 'n', 9: 'b', 10: 'r', 11: 'q', 12: 'k'}
+    return mapping.get(num, '.')
+
+def tensor_to_fen(tensor):
+    
+    fen = ''
+    for row in tensor:
+        empty_count = 0
+        for cell in row:
+            
+            cell_value = cell.item()
+            piece = number_to_piece(cell_value)
+            
+            if piece == '.':
+                empty_count += 1
+            else:
+                if empty_count > 0:
+                    fen += str(empty_count)
+                    empty_count = 0
+                fen += piece
+        if empty_count > 0:
+            fen += str(empty_count)
+        fen += '/'
+    return fen[:-1]  # Remove the last '/'
+
+def print_chessboard_from_fen(fen):
+    board = chess.Board(fen)  # Append a standard ending for completeness
+    board_svg = chess.svg.board(board, size=350)
+    display(SVG(board_svg))
+
+def bitboards_to_array(bb: np.ndarray) -> np.ndarray:
+    bb = np.asarray(bb, dtype=np.uint64)[:, np.newaxis]
+    s = 8 * np.arange(7, -1, -1, dtype=np.uint64)
+    b = (bb >> s).astype(np.uint8)
+    b = np.unpackbits(b, bitorder="little")
+    return b.reshape(-1, 8, 8)
+
+
+def fen_to_bitboard(fen):
+    board = chess.Board(fen)   
+    black, white = board.occupied_co
+
+    bitboards = np.array([
+        black & board.pawns,
+        black & board.knights,
+        black & board.bishops,
+        black & board.rooks,
+        black & board.queens,
+        black & board.kings,
+        white & board.pawns,
+        white & board.knights,
+        white & board.bishops,
+        white & board.rooks,
+        white & board.queens,
+        white & board.kings,
+    ], dtype=np.uint64)
+
+    board_array = bitboards_to_array(bitboards)
+    return(torch.tensor(board_array))
+
+
+def bitboard_to_fen(bitboard):
+    
+    piece_order = ['p', 'n', 'b', 'r', 'q', 'k', 'P', 'N', 'B', 'R', 'Q', 'K']
+
+    if isinstance(bitboard, torch.Tensor):
+        bitboard = bitboard.numpy()
+
+    board = chess.Board(None)
+    
+    for i, piece_symbol in enumerate(piece_order):
+       
+        piece_bitboard = bitboard[i]
+
+        for row in range(8):
+            for col in range(8):
+                square = chess.square(col, 7 - row)
+                if piece_bitboard[row, col]:
+                    piece = chess.Piece.from_symbol(piece_symbol)
+                    board.set_piece_at(square, piece)
+
+    
+    return board.fen()
+
+def process_tensor(tensor):
+    # Step 1: Set negative values to 0
+    tensor = torch.clamp(tensor, min=0)
+
+    # Step 2: Set the maximum positive value to 1 and others to 0
+    for i in range(tensor.shape[1]):  # Iterate through 8x8 matrices
+        for j in range(tensor.shape[2]):
+            # Flatten the (12, ) vector, find the index of the max value
+            max_val_index = torch.argmax(tensor[:, i, j])
+            max_val = tensor[max_val_index, i, j]
+
+            if max_val > 0:  # Check if the maximum value is positive
+                # Set all values to 0
+                tensor[:, i, j] = 0
+                # Set only the maximum value to 1
+                tensor[max_val_index, i, j] = 1
+
+    return tensor
+# helpers functions
+
+def exists(x):
+    return x is not None
+
+def default(val, d):
+    if exists(val):
+        return val
+    return d() if callable(d) else d
+
+def cycle(dl):
+    while True:
+        for data in dl:
+            yield data
+
+def has_int_squareroot(num):
+    return (math.sqrt(num) ** 2) == num
+
+def num_to_groups(num, divisor):
+    groups = num // divisor
+    remainder = num % divisor
+    arr = [divisor] * groups
+    if remainder > 0:
+        arr.append(remainder)
+    return arr
+
+
+# bit diffusion class
+
+def log(t, eps = 1e-20):
+    return torch.log(t.clamp(min = eps))
+
+def right_pad_dims_to(x, t):
+    padding_dims = x.ndim - t.ndim
+    if padding_dims <= 0:
+        return t
+    return t.view(*t.shape, *((1,) * padding_dims))
+
+def beta_linear_log_snr(t):
+    return -torch.log(expm1(1e-4 + 10 * (t ** 2)))
+
+def alpha_cosine_log_snr(t, s: float = 0.008):
+    return -log((torch.cos((t + s) / (1 + s) * math.pi * 0.5) ** -2) - 1, eps = 1e-5) # not sure if this accounts for beta being clipped to 0.999 in discrete version
+
+def log_snr_to_alpha_sigma(log_snr):
+    return torch.sqrt(torch.sigmoid(log_snr)), torch.sqrt(torch.sigmoid(-log_snr))
+
+class BitDiffusion(nn.Module):
+    def __init__(
+        self,
+        model,
+        *,
+        image_size,
+        timesteps = 1000,
+        use_ddim = False,
+        noise_schedule = 'cosine',
+        time_difference = 0.,
+        bit_scale = 1.
+    ):
+        super().__init__()
+        self.model = model
+        self.channels = self.model.channels
+
+        self.image_size = image_size
+
+        if noise_schedule == "linear":
+            self.log_snr = beta_linear_log_snr
+        elif noise_schedule == "cosine":
+            self.log_snr = alpha_cosine_log_snr
+        else:
+            raise ValueError(f'invalid noise schedule {noise_schedule}')
+
+        self.bit_scale = bit_scale
+
+        self.timesteps = timesteps
+        self.use_ddim = use_ddim
+
+        # proposed in the paper, summed to time_next
+        # as a way to fix a deficiency in self-conditioning and lower FID when the number of sampling timesteps is < 400
+
+        self.time_difference = time_difference
+
+    @property
+    def device(self):
+        return next(self.model.parameters()).device
+
+    def get_sampling_timesteps(self, batch, *, device):
+        times = torch.linspace(1., 0., self.timesteps + 1, device = device)
+        times = repeat(times, 't -> b t', b = batch)
+        times = torch.stack((times[:, :-1], times[:, 1:]), dim = 0)
+        times = times.unbind(dim = -1)
+        return times
+
+    @torch.no_grad()
+    def ddpm_sample(self, shape, time_difference = None):
+        batch, device = shape[0], self.device
+
+        time_difference = default(time_difference, self.time_difference)
+
+        time_pairs = self.get_sampling_timesteps(batch, device = device)
+
+        img = torch.randn(shape, device=device)
+
+        x_start = None
+
+        for time, time_next in tqdm(time_pairs, desc = 'sampling loop time step', total = self.timesteps):
+
+            # add the time delay
+
+            time_next = (time_next - self.time_difference).clamp(min = 0.)
+
+            noise_cond = self.log_snr(time)
+
+            # get predicted x0
+
+            x_start = self.model(img, noise_cond, x_start)
+
+            # clip x0
+
+            x_start.clamp_(-self.bit_scale, self.bit_scale)
+
+            # get log(snr)
+
+            log_snr = self.log_snr(time)
+            log_snr_next = self.log_snr(time_next)
+            log_snr, log_snr_next = map(partial(right_pad_dims_to, img), (log_snr, log_snr_next))
+
+            # get alpha sigma of time and next time
+
+            alpha, sigma = log_snr_to_alpha_sigma(log_snr)
+            alpha_next, sigma_next = log_snr_to_alpha_sigma(log_snr_next)
+
+            # derive posterior mean and variance
+
+            c = -expm1(log_snr - log_snr_next)
+
+            mean = alpha_next * (img * (1 - c) / alpha + c * x_start)
+            variance = (sigma_next ** 2) * c
+            log_variance = log(variance)
+
+            # get noise
+
+            noise = torch.where(
+                rearrange(time_next > 0, 'b -> b 1 1 1'),
+                torch.randn_like(img),
+                torch.zeros_like(img)
+            )
+
+            img = mean + (0.5 * log_variance).exp() * noise
+
+        return img
+
+    @torch.no_grad()
+    def ddim_sample(self, shape, time_difference = None):
+        batch, device = shape[0], self.device
+
+        time_difference = default(time_difference, self.time_difference)
+
+        time_pairs = self.get_sampling_timesteps(batch, device = device)
+
+        img = torch.randn(shape, device = device)
+
+        x_start = None
+
+        for times, times_next in tqdm(time_pairs, desc = 'sampling loop time step'):
+
+            # add the time delay
+
+            times_next = (times_next - time_difference).clamp(min = 0.)
+
+            # get times and noise levels
+
+            log_snr = self.log_snr(times)
+            log_snr_next = self.log_snr(times_next)
+
+            padded_log_snr, padded_log_snr_next = map(partial(right_pad_dims_to, img), (log_snr, log_snr_next))
+
+            alpha, sigma = log_snr_to_alpha_sigma(padded_log_snr)
+            alpha_next, sigma_next = log_snr_to_alpha_sigma(padded_log_snr_next)
+
+            # predict x0
+
+            x_start = self.model(img, log_snr, x_start)
+
+            # clip x0
+
+            x_start.clamp_(-self.bit_scale, self.bit_scale)
+
+            # get predicted noise
+
+            pred_noise = (img - alpha * x_start) / sigma.clamp(min = 1e-8)
+
+            # calculate x next
+
+            img = x_start * alpha_next + pred_noise * sigma_next
+
+        return img
+
+    @torch.no_grad()
+    def sample(self, batch_size = 16):
+        image_size, channels = self.image_size, self.channels
+        sample_fn = self.ddpm_sample if not self.use_ddim else self.ddim_sample
+        return sample_fn((batch_size, channels, image_size, image_size))
+
+    def forward(self, img, *args, **kwargs):
+        batch, c, h, w, device, img_size, = *img.shape, img.device, self.image_size
+        assert h == img_size and w == img_size, f'height and width of image must be {img_size}'
+
+        # sample random times
+
+        times = torch.zeros((batch,), device = device).float().uniform_(0, 1.)
+
+        # noise sample
+        noise = torch.randn_like(img)
+
+        noise_level = self.log_snr(times)
+        padded_noise_level = right_pad_dims_to(img, noise_level)
+        alpha, sigma =  log_snr_to_alpha_sigma(padded_noise_level)
+
+        noised_img = alpha * img + sigma * noise
+
+        # if doing self-conditioning, 50% of the time, predict x_start from current set of times
+        # and condition with unet with that
+        # this technique will slow down training by 25%, but seems to lower FID significantly
+
+        self_cond = None
+
+        if torch.rand((1)) < 0.5:
+            with torch.no_grad():
+                self_cond = self.model(noised_img, noise_level).detach_()
+    
+        # predict and take gradient step
+
+        pred = self.model(noised_img, noise_level, self_cond)
+
+        return F.mse_loss(pred, img)
+
+# dataset classes
+
+class ChessDataset(Dataset):
+    def __init__(self, fen_data):
+        super().__init__()
+        self.fen_data = fen_data 
+
+    def __len__(self):
+        return len(self.fen_data)
+
+    def __getitem__(self, index):
+        fen = self.fen_data[index]
+        tensor = fen_to_bitboard(fen) 
+        transformed_tensor = 2 *  torch.tensor(tensor, dtype=torch.float32) - 1
+      
+        return transformed_tensor
+
+# trainer class
+
+class Trainer(object):
+    def __init__(
+        self,
+        diffusion_model,
+        fen_data, 
+        *,
+        train_batch_size = 16,
+        gradient_accumulate_every = 1,
+        augment_horizontal_flip = True,
+        train_lr = 1e-4,
+        train_num_steps = 100000,
+        ema_update_every = 10,
+        ema_decay = 0.995,
+        adam_betas = (0.9, 0.99),
+        save_and_sample_every = 1000,
+        num_samples = 25,
+        results_folder = './results',
+        amp = False,
+        mixed_precision_type = 'fp16',
+        split_batches = True,
+        pil_img_type = None
+    ):
+        super().__init__()
+
+        self.accelerator = Accelerator(
+            split_batches = split_batches,
+            mixed_precision = mixed_precision_type if amp else 'no'
+        )
+
+        self.model = diffusion_model
+
+        assert has_int_squareroot(num_samples), 'number of samples must have an integer square root'
+        self.num_samples = num_samples
+        self.save_and_sample_every = save_and_sample_every
+
+        self.batch_size = train_batch_size
+        self.gradient_accumulate_every = gradient_accumulate_every
+
+        self.train_num_steps = train_num_steps
+        self.image_size = diffusion_model.image_size
+
+        # dataset and dataloader
+
+        self.ds = ChessDataset(fen_data)
+        dl = DataLoader(self.ds, batch_size=train_batch_size, shuffle=True, pin_memory=True, num_workers=cpu_count())
+
+        dl = self.accelerator.prepare(dl)
+        self.dl = cycle(dl)
+
+        # optimizer
+
+        self.opt = Adam(diffusion_model.parameters(), lr = train_lr, betas = adam_betas)
+
+        # for logging results in a folder periodically
+
+        if self.accelerator.is_main_process:
+            self.ema = EMA(diffusion_model, beta = ema_decay, update_every = ema_update_every)
+
+            self.results_folder = Path(results_folder)
+            self.results_folder.mkdir(exist_ok = True)
+
+        # step counter state
+
+        self.step = 0
+
+        # prepare model, dataloader, optimizer with accelerator
+
+        self.model, self.opt = self.accelerator.prepare(self.model, self.opt)
+
+    def save(self, milestone):
+        if not self.accelerator.is_local_main_process:
+            return
+
+        data = {
+            'step': self.step,
+            'model': self.accelerator.get_state_dict(self.model),
+            'opt': self.opt.state_dict(),
+            'ema': self.ema.state_dict(),
+            'scaler': self.accelerator.scaler.state_dict() if exists(self.accelerator.scaler) else None
+        }
+
+        torch.save(data, str(self.results_folder / f'model-{milestone}.pt'))
+
+    def load(self, milestone):
+        data = torch.load(str(self.results_folder / f'model-{milestone}.pt'))
+
+        model = self.accelerator.unwrap_model(self.model)
+        model.load_state_dict(data['model'])
+
+        self.step = data['step']
+        self.opt.load_state_dict(data['opt'])
+        self.ema.load_state_dict(data['ema'])
+
+        if exists(self.accelerator.scaler) and exists(data['scaler']):
+            self.accelerator.scaler.load_state_dict(data['scaler'])
+
+    def train(self):
+        accelerator = self.accelerator
+        device = accelerator.device
+
+        with tqdm(initial = self.step, total = self.train_num_steps, disable = not accelerator.is_main_process) as pbar:
+
+            while self.step < self.train_num_steps:
+
+                total_loss = 0.
+
+                for _ in range(self.gradient_accumulate_every):
+                    data = next(self.dl).to(device)
+
+                    with self.accelerator.autocast():
+                        loss = self.model(data)
+                        loss = loss / self.gradient_accumulate_every
+                        total_loss += loss.item()
+
+                    self.accelerator.backward(loss)
+
+                pbar.set_description(f'loss: {total_loss:.4f}')
+
+                accelerator.wait_for_everyone()
+
+                self.opt.step()
+                self.opt.zero_grad()
+
+                accelerator.wait_for_everyone()
+
+                if accelerator.is_main_process:
+                    self.ema.to(device)
+                    self.ema.update()
+
+                    if self.step != 0 and self.step % self.save_and_sample_every == 0:
+                        self.ema.ema_model.eval()
+
+                        with torch.no_grad():
+                            # Sample one batch from the model
+                            sample_tensor = self.ema.ema_model.sample(batch_size=1)
+                            
+                            transformed_tensor = process_tensor(sample_tensor[0].squeeze())
+                            fen = bitboard_to_fen(transformed_tensor)
+
+                            # Print the chessboard
+                            print_chessboard_from_fen(fen)
+
+                self.step += 1
+                pbar.update(1)
+
+        accelerator.print('training complete')
